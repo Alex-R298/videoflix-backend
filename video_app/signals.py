4ab2b8939222c96b89592276f5990a7102fda469
Alex-R298 <alexreitz298@gmail.com>
2026-05-11 19:46:56 +0200

    Add signal handling for video deletion to remove associated files

--- video_app/signals.py
+++ video_app/signals.py
@@ -1,6 +1,7 @@
 from .models import Video
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
+import os
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
@@ -8,6 +9,17 @@ def video_post_save(sender, instance, created, **kwargs):
 
     if created:
         print('New object created')
+
+
+@receiver(post_delete, sender=Video)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `Video` object is deleted.
+    """
+    if instance.video_file:
+        if os.path.isfile(instance.video_file.path):
+            os.remove(instance.video_file.path)
 
 
 
