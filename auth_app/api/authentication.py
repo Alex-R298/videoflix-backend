6dc2ac6c2d574fae9a292a165e729535d22b29ab
Alex-R298 <alexreitz298@gmail.com>
2026-05-12 13:19:09 +0200

    Add CookieJWTAuthentication class for JWT token handling from HttpOnly cookies

---
+++ auth_app/api/authentication.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+class CookieJWTAuthentication(JWTAuthentication):
+    """JWT-Authentication, die das Access-Token aus einem HttpOnly-Cookie liest."""
+
+    def authenticate(self, request):
+        raw_token = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE'])
+        if raw_token is None:
+            return None
+        validated_token = self.get_validated_token(raw_token)
+        user = self.get_user(validated_token)
+        return user, validated_token
